0f5abba6186eb15c261751917a4c9b67d2e5510c
Khamphanh Phongsavath <Khamphanh.Phongsavath@osumc.edu>
2022-03-27 16:25:31 -0400

    initial

---
+++ Powershell/SelfSignedCert.ps1
@@ -0,0 +1,60 @@
+
+$CA_CERT_NAME = 'Cancer Sitecore CA';
+$ENVIONMENT = 'qa';
+$CA_Thumbprint = '';
+$DOMAIN = 'osumc.edu';
+
+#Make Sure location exists
+$CA_CERT_TEMP_LOC = "C:\certificates"
+
+$CERTS_TO_CREATE = @(
+"cancer-processing-$($ENVIONMENT).$($DOMAIN)",
+);
+
+if(Text-Path $CA_CERT_TEMP_LOC){
+}else{
+    New-Item $CA_CERT_TEMP_LOC -ItemType Directory
+
+}
+
+
+Write-host "Certificates to create" -ForegroundColor Cyan
+$CERTS_TO_CREATE 
+
+
+$rootca = Get-ChildItem cert:\CurrentUser\my | Where-Object {$_.Subject -eq "CN=$($CA_CERT_NAME)"}
+if($rootca -eq $null){
+
+Write-host "Creating CA Certificate $($CA_CERT_NAME) ..." -ForegroundColor Green
+
+$rootcert = New-SelfSignedCertificate -CertStoreLocation cert:\CurrentUser\My -DnsName $CA_CERT_NAME -KeyUsage CertSign
+
+Write-host "Certificate Thumbprint: $($rootcert.Thumbprint)"
+$CA_Thumbprint =  $rootcert.Thumbprint
+
+#This needs to be added to Trusted Root
+Export-Certificate -Cert $rootcert -FilePath "$($CA_CERT_TEMP_LOC)\$($CA_CERT_NAME).cer"
+
+#Imports certificate to Trusted Publishers (Requires "Run as Administrator")
+Import-Certificate -FilePath "$($CA_CERT_TEMP_LOC)\$($CA_CERT_NAME).cer" -CertStoreLocation Cert:\LocalMachine\Root
+
+#the thumbprint of need to be changed to your root certificate. 
+$rootca = Get-ChildItem cert:\CurrentUser\my | Where-Object {$_.Thumbprint -eq $rootcert.Thumbprint}
+
+}else{
+
+$CA_Thumbprint = $rootca.Thumbprint
+
+Write-host "Certificate With Name Exists: $($CA_CERT_NAME)" -ForegroundColor Yellow
+Write-host "Certificate Thumbprint: $($rootca.Thumbprint)"
+
+
+}
+
+
+foreach ($CERT_NAME in $CERTS_TO_CREATE)
+{
+  New-SelfSignedCertificate -certstorelocation cert:\LocalMachine\My -dnsname $CERT_NAME -FriendlyName $CERT_NAME -Signer $rootca -NotAfter $([datetime]::now.AddYears(5))
+}
+
+
